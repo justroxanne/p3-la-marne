9690348eb9574b9d88836f5bd8bffa7b2cc0df06
Axelastach <axelpicaut@orange.fr>
2023-06-05 09:38:53 +0200

    BackAdresses done

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,7 +1,10 @@
 const UserController = require('./UserController');
 const AdminController = require('./AdminController');
+const AdressesController = require('./AdressesController');
+
 
 module.exports = {
   UserController,
+  AdressesController,
   AdminController,
 };
---
+++ api/src/models/AdressesModel.js
@@ -0,0 +1,9 @@
+const BaseModel = require('./BaseModel');
+
+class AdressesModel extends BaseModel {
+  constructor() {
+    super('adresses');
+  }
+}
+
+module.exports = AdressesModel;
