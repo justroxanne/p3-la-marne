942cb9ed1066cc9ab36e4d374ad633829beeefdf
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-06-06 11:47:09 +0200

    delete commented code

--- api/src/models/UserModel.js
+++ api/src/models/UserModel.js
@@ -4,34 +4,6 @@ class UserModel extends BaseModel {
   constructor() {
     super('users');
   }
-
-  // updateUser() {
-  //   const {
-  //     company_name,
-  //     firstname,
-  //     lastname,
-  //     siret,
-  //     telephone,
-  //     email,
-  //     password,
-  //     website,
-  //   } = req.body;
-  //   const id = parseInt(req.params.id);
-
-  //   return this.db.query(
-  //     `UPDATE users SET raison_sociale = ?, firstname = ?, lastname = ?, siret = ?, telephone = ?, email = ?, password = ?, website = ?`,
-  //     [
-  //       raison_sociale,
-  //       firstname,
-  //       lastname,
-  //       siret,
-  //       telephone,
-  //       email,
-  //       password,
-  //       website,
-  //     ]
-  //   );
-  // }
 }
 
 module.exports = UserModel;
