803c865ab16a08f2b99bf6bb179ee0a5e1e71953
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-06-07 10:30:56 +0200

    ras

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserDashboard from './users/pages/UserDashboard';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/userDashboard' element={<UserDashboard />} />
+        <Route path='/userdashboard' element={<UserDashboard />} />
       </Routes>
     </Router>
   );
