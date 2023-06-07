35b4e5e65a1efaee7df290ccb967d9fad91d25d1
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-06-07 10:24:42 +0200

    user dashboard init

---
+++ client/src/users/pages/UserDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './userDashboard.css';
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1>Hello User!</h1>
+    </div>
+  );
+};
+
+export default UserDashboard;
