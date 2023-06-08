f09d505b8487eef9d4b4a4356919466645981b74
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-06-08 11:38:59 +0200

    user card done

--- client/src/users/pages/UserDashboard.jsx
+++ client/src/users/pages/UserDashboard.jsx
@@ -6,7 +6,32 @@ const UserDashboard = () => {
   return (
     <div className='user-dashboard'>
       <UserCard />
-      <button>Faire une demande</button>
+      <div className='my-steps'>
+        Mes demandes en cours
+        <hr />
+        <table>
+          <thead>
+            <tr>
+              <th>Numéro de demande</th>
+              <th>Date</th>
+              <th>Label</th>
+              <th>Avancement</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>5776537900</td>
+              <td>07/06/2023</td>
+              <td>AOP</td>
+              <td>En cours</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className='my-labels'>
+        Mes labels
+        <hr />
+      </div>
     </div>
   );
 };
